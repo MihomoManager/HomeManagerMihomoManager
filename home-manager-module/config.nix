70c7edde52bc2bd5890c3c036549a06d1d1091d8
yueyinqiu <yueyinqiu@outlook.com>
2026-09-20 14:31:52 +0800

    f

--- home-manager-module/config.nix
+++ home-manager-module/config.nix
@@ -79,13 +79,7 @@ in
         Description = "home-manager-mihomo-manager Service ${name}";
         After = [ "network-online.target" ];
         Wants = [ "network-online.target" ];
-        X-Restart-Triggers = [
-          (builtins.path {
-            path = item.configuration;
-            name = "home-manager-mihomo-manager-${name}-configuration";
-            recursive = true;
-          })
-        ];
+        X-Restart-Triggers = [ "${item.configuration}" ];
       };
       Install.WantedBy = [ "default.target" ];
       Service = {
